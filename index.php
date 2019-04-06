22053b454a7954564ca356100375b46612a8128b
Muhammad Ferry Afrizal <ferryafrizal@outlook.co.id>
2019-04-06 11:30:47 +0800

    Update index.php
    Submission 1 - Azure Web App & SQL Database

--- index.php
+++ index.php
@@ -33,8 +33,6 @@
       </form>
       
       <?php
-
-	   /*
       $host = "tcp:bukabuku.database.windows.net, 1433";
       $user = "mafrizal";
       $pass = "Timpakul2016+";
@@ -67,7 +65,6 @@
          }
          echo "<h3>Your're registered!</h3>";
       }
-      */
       ?>
       <table class="table table-responsive mt-3">
          <thead>
@@ -88,10 +85,10 @@
    </div>
    <!-- Optional JavaScript -->
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   <!--<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   -->
+   
 </body>
 
 </html>
