689ae094eb06cb9db18eb030d0ffe49e2fd8d462
Muhammad Ferry Afrizal <ferryafrizal@outlook.co.id>
2019-04-06 21:50:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
    <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>jQuery UI Datepicker - Default functionality</title>
    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
    <link rel="stylesheet" href="/resources/demos/style.css">
    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -21,7 +20,7 @@
 		} );
 	</script>
 
-   <title>Submission Azure</title>
+   <title>Bukabuku</title>
 </head>
 
 <body>
