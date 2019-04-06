e70414f733d562c3479e1e15797cdecb1e9eeaec
crips <ferryafrizal@outlook.co.id>
2019-04-06 21:54:28 +0800

    lagi

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
    <!-- Required meta tags -->
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>jQuery UI Datepicker - Default functionality</title>
+
+   <title>Bukabuku</title>
+
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
    <link rel="stylesheet" href="/resources/demos/style.css">
    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -20,8 +20,6 @@
 			$( "#datepicker" ).datepicker();
 		} );
 	</script>
-
-   <title>Submission Azure</title>
 </head>
 
 <body>
@@ -98,6 +96,7 @@
             }
       }
       ?>
+
       <table class="table table-responsive mt-3">
          <thead>
             <tr>
