5dc2157775f519f9ebeecdd8d152f1da92a89cf7
crips <ferryafrizal@outlook.co.id>
2019-04-10 17:29:50 +0800

    koneksi

--- config.php
+++ config.php
@@ -17,8 +17,9 @@
     } catch (Exception $e) {
 		echo "Failed : " . $e;
     }
-	
+	/*
 	$connectionInfo = array("UID" => "mafrizal@bukabuku", "pwd" => "Timpakul2016+", "Database" => "bukabuku", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 	$serverName = "tcp:bukabuku.database.windows.net, 1433";
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	*/
 ?>
